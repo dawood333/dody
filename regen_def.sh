95bd4ab14880a16d01aac63273ad3dd84d1e0d03
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-09-17 08:25:25 +0000

    regen_script: Add Support for `ares`

--- regen_def.sh
+++ regen_def.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-DEFCONFIG="chopin_user_defconfig"
+if [ $1 = "--ares" ]; then
+		export DEVICE=ares
+		export DEFCONFIG=ares_user_defconfig
+	else
+		export DEVICE=chopin
+		export DEFCONFIG=chopin_user_defconfig
+fi
 
 make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
 cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
 git add arch/arm64/configs/"${DEFCONFIG}"
-git commit -m "chopin: Regenerate"
+git commit -m "$DEVICE: Sync config with source"
 echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
