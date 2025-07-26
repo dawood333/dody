1dd54b8168c3b5af9a09ee3949eaf1560357d425
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-07-27 06:49:06 +0900

    android: binder: Fix Redefinition Error

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -47,8 +47,7 @@ enum {
 	BINDER_DEBUG_USER_ERROR             = 1U << 0,
 	BINDER_DEBUG_OPEN_CLOSE             = 1U << 1,
 	BINDER_DEBUG_BUFFER_ALLOC           = 1U << 2,
-	BINDER_DEBUG_BUFFER_ALLOC_ASYNC     = 1U << 3,
-	BINDER_DEBUG_USER_ERROR             = 1U << 4,
+	BINDER_DEBUG_BUFFER_ALLOC_ASYNC     = 1U << 3
 };
 static uint32_t binder_alloc_debug_mask = BINDER_DEBUG_USER_ERROR;
 
